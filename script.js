96afbd60f0b9ab084db5df56f2a18b835a80ceee
Shelo2001 <100028515+Shelo2001@users.noreply.github.com>
2023-01-12 16:31:55 +0400

    finished responsive design

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ accordions.forEach((accordion) => {
     }
   })
 })
+
+const cardButton2 = document.querySelector('.cardButton2')
+cardButton2.addEventListener('click', (e) => {
+  document.querySelector('.review-cards').style.transform = 'translate(-350px)'
+  cardButton2.style.color = '#197aff'
+  document.getElementById('cardButtons3').style.color = '#cbced6'
+  document.getElementById('cardButtons1').style.color = '#cbced6'
+})
+const cardButton3 = document.querySelector('.cardButton3')
+cardButton3.addEventListener('click', (e) => {
+  document.querySelector('.review-cards').style.transform = 'translate(-700px)'
+  cardButton3.style.color = '#197aff'
+  document.getElementById('cardButtons2').style.color = '#cbced6'
+  document.getElementById('cardButtons1').style.color = '#cbced6'
+})
+const cardButton1 = document.querySelector('.cardButton1')
+cardButton1.addEventListener('click', (e) => {
+  document.querySelector('.review-cards').style.transform = 'translate(0px)'
+  cardButton1.style.color = '#197aff'
+  document.getElementById('cardButtons2').style.color = '#cbced6'
+  document.getElementById('cardButtons3').style.color = '#cbced6'
+})
