c39218c1afbee2b94736976397cdbab00658f0b3
Shelo2001 <100028515+Shelo2001@users.noreply.github.com>
2023-01-12 17:07:49 +0400

    final commit

--- script.js
+++ script.js
@@ -13,21 +13,27 @@ accordions.forEach((accordion) => {
 
 const cardButton2 = document.querySelector('.cardButton2')
 cardButton2.addEventListener('click', (e) => {
-  document.querySelector('.review-cards').style.transform = 'translate(-350px)'
+  document.getElementById('reviewcard2').style.transform = 'translate(-350px)'
+  document.getElementById('reviewcard3').style.transform = 'translate(-350px)'
+  document.getElementById('reviewcard4').style.transform = 'translate(-350px)'
   cardButton2.style.color = '#197aff'
   document.getElementById('cardButtons3').style.color = '#cbced6'
   document.getElementById('cardButtons1').style.color = '#cbced6'
 })
 const cardButton3 = document.querySelector('.cardButton3')
 cardButton3.addEventListener('click', (e) => {
-  document.querySelector('.review-cards').style.transform = 'translate(-700px)'
+  document.getElementById('reviewcard2').style.transform = 'translate(-700px)'
+  document.getElementById('reviewcard3').style.transform = 'translate(-700px)'
+  document.getElementById('reviewcard4').style.transform = 'translate(-700px)'
   cardButton3.style.color = '#197aff'
   document.getElementById('cardButtons2').style.color = '#cbced6'
   document.getElementById('cardButtons1').style.color = '#cbced6'
 })
 const cardButton1 = document.querySelector('.cardButton1')
 cardButton1.addEventListener('click', (e) => {
-  document.querySelector('.review-cards').style.transform = 'translate(0px)'
+  document.getElementById('reviewcard2').style.transform = 'translate(-0px)'
+  document.getElementById('reviewcard3').style.transform = 'translate(-0px)'
+  document.getElementById('reviewcard4').style.transform = 'translate(-0px)'
   cardButton1.style.color = '#197aff'
   document.getElementById('cardButtons2').style.color = '#cbced6'
   document.getElementById('cardButtons3').style.color = '#cbced6'
